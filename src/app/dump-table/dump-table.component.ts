3339683e737ad30114835b2c10d898ac9ef3c6c0
WilhelmRosental <wilhelm.rosental@ik.me>
2025-04-16 01:05:44 +0200

    update

--- src/app/dump-table/dump-table.component.ts
+++ src/app/dump-table/dump-table.component.ts
@@ -3,11 +3,25 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DumpDataService } from '../dump-data.service';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
     selector: 'app-dump-table',
     standalone: true,
-    imports: [CommonModule, FormsModule, MatCardModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatTableModule,
+        MatToolbarModule,
+        MatButtonModule
+    ],
     templateUrl: './dump-table.component.html',
     styleUrls: ['./dump-table.component.css']
 })
